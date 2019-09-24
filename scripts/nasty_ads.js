bf7364ea8d089c31d91a24ff1fc3a38e8c65ddee
SadDrummer <norganclorr@gmail.com>
2019-09-24 20:00:11 +0300

    Finished nasty ad

---
+++ scripts/nasty_ads.js
@@ -0,0 +1,18 @@
+let ad = document.getElementById("nasty_ads_block"),
+	colors = ["#00C7D5", "#20A020", "yellow"],
+	i = 0;
+let timer = setInterval(()=>{
+	ad.style.backgroundColor = colors[i];
+	i = ++i % 3;
+}, 1000);
+
+ad.addEventListener("click", ()=>{
+	window.location = "http://google.com";
+});
+let close = ad.querySelector("div");
+close.addEventListener("mousedown", function(e){
+	ad.style.marginTop = "-41px";
+});
+close.addEventListener("mouseup",function(e){
+	ad.remove();
+});
